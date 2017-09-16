063910ebdddfe10d765acc49c6886d6649fc8f9e
Mike Holzbach <maholzbach@gmail.com>
2017-09-16 12:00:30 -0400

    Study

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './css/style.css';
-import StorePicker from './components/StorePicker';
+import App from './components/App';
 
 render (
-  <StorePicker />,
+  <App />,
   document.querySelector('#main')
 )
