0cccfbe4e7cea4cd0a73576848f8ee97b7575851
Mike Holzbach <maholzbach@gmail.com>
2017-09-25 18:18:03 -0400

    study

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -28,6 +28,14 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({
+      order: order
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +43,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
